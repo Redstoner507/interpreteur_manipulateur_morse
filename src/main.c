8cf7f184f487be6854386848dd270cae8be4a619
Yann Stoll-Geyer <stollgeyer.yann@gmail.com>
2025-10-12 15:32:12 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,8 +5,14 @@ int main(void) {
 
 	GPIO gpio;
 
-	gpio_open(&gpio); // Ouvre les GPIOs pour la lecture
+	if (gpio_open(&gpio) == -1) // Ouvre les GPIOs pour la lecture
+		return EXIT_FAILURE; 
 
+	while (1)
+	{
+		
+	}
+	
 	gpio_close(&gpio); // Libère les GPIOs ouverts
 
 	return EXIT_SUCCESS;
