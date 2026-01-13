75aee449fb82dd931cfe0cf730728d0b2820ef20
STOLL-GEYER YANN <yann.stoll-geyer@etu.unistra.fr>
2026-01-13 16:04:48 +0100

    Modif de la lecture du signal dans interprete.c + Changement de structure pour le morse

--- include/morse.h
+++ include/morse.h
@@ -5,7 +5,7 @@
 
 static const int UNITE_TEMPS = 60;
 /*
-20 WPM -> une nité = 60ms
+20 WPM -> une unité = 60ms
 Point = 60 ms -> u*1 (Court)
 Trait = 180 ms -> u*3 (Long)
 Espace entre symbole = 60ms u*1
@@ -13,28 +13,24 @@ Espace entre lettres = 180 ms u*3
 Espace entre mots = 420 ms u*7
 */
 
-// Enumération pour lettres, chiffres et ponctuation courante
-typedef enum {
-    A, B, C, D, E, F, G, H, I, J,
-    K, L, M, N, O, P, Q, R, S, T,
-    U, V, W, X, Y, Z,
-    ZERO, UN, DEUX, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF,
-    POINT, VIRGULE, QUESTION, APOSTROPHE, EXCLAMATION,
-    SLASH, PARENTHESE_OUV, PARENTHESE_FERM, ET, DEUX_POINTS,
-    POINT_VIRGULE, EGAL, PLUS, MOINS, UNDERSCORE, GUILLEMET, DOLLAR, ARROBASE,
-    NB_CARACTERES
-} Caractere;
+typedef struct {
+    char letter;
+    const char *morse;
+} MorseEntry;
 
-// Tableau associant chaque caractère à son code Morse officiel
-static const char* morseCode[NB_CARACTERES] = {
-    // Lettres
-    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
-    "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
-    "..-", "...-", ".--", "-..-", "-.--", "--..",
-    // Chiffres
-    "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-    // Ponctuation
-    ".-.-.-", "--..--", "..--..", ".----.", "-.-.--",
-    "-..-.", "-.--.", "-.--.-", ".-...", "---...", "-.-.-.", "-...-", ".-.-.", "-....-", "..--.-", ".-..-.", "...-..-", ".--.-."
+static const MorseEntry morseTable[] = {
+    {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+    {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+    {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+    {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+    {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"}, {'Z', "--.."},
+    {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"},
+    {'5', "....."}, {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."},
+    {'.', ".-.-.-"}, {',', "--..--"}, {'?', "..--.."}, {'\'', ".----."}, {'!', "-.-.--"},
+    {'/', "-..-."}, {'(', "-.--."}, {')', "-.--.-"}, {'&', ".-..."}, {':', "---..."},
+    {';', "-.-.-."}, {'=', "-...-"}, {'+', ".-.-."}, {'-', "-....-"}, {'_', "..--.-"},
+    {'"', ".-..-."}, {'$', "...-..-"}, {'@', ".--.-."}
 };
+
+static const int NB_CARACTERES = sizeof(morseTable)/sizeof(MorseEntry);
 
--- src/interprete.c
+++ src/interprete.c
@@ -3,14 +3,15 @@
 int boucleLecture(int fd) {
     
     ssize_t n;
-    double duration;
-    char buffer[7];
+    singalRecu signal;
+    char buffer[8];
 
-    while ((n = read(fd, &duration, sizeof(duration))) > 0)
+    while ((n = read(fd, &signal, sizeof(singalRecu))) > 0)
     {
         
     }
 
-    if (n < 0) return -1;  
+    if (n < 0) return -1;
+      
     return 0;
 }
